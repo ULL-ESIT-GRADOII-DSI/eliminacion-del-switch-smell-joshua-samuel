9ea17b6c99bf8979eff8f60e118046ff16f3cfb2
joshuape <alu0100818819@ull.edu.es>
2016-03-10 10:28:37 +0000

    completando clase medida

--- assets/js/medida.js
+++ assets/js/medida.js
@@ -17,9 +17,52 @@
                                      '(\\s*) \n' +
                                      '(?<para> [fckFCK]) # para \n' +
                                      '(\\s*)$','x');
-    
+    Medida.convertir = function(value) { 
+    var valor = XRegExp.exec(value, Medida.prototype.REGEXP); 
+     if (valor) {
+      var numero = valor.val,
+          tipo   = valor.tip.toLowerCase(),
+          destino = valor.para.toLowerCase();
+      numero = parseFloat(numero);
+      console.log("Valor: " + numero + ", Tipo: " + tipo + ", To: " + destino);
+
+      switch (tipo) {
+        case 'c':
+          var celsius = new Celsius(numero);
+          if(destino == 'c')
+            return numero.toFixed(2) + " Celsius";
+          if(destino == 'f')
+            return celsius.toFahrenheit().toFixed(2) + " Fahrenheit";
+          if(destino == 'k')
+            return celsius.toKelvin().toFixed(2) + " Kelvin";
+          break;
+        case 'f':
+          var fahrenheit = new Fahrenheit(numero);
+          if(destino == 'f')
+            return numero.toFixed(2) + " Fahrenheit";
+          if(destino == 'c')
+            return fahrenheit.toCelsius().toFixed(2) + " Celsius";
+          if(destino == 'k')
+            return fahrenheit.toKelvin().toFixed(2) + " Kelvin";
+          break;
+        case 'k':
+          var kelvin = new Kelvin(numero);
+          if(destino == 'k')
+            return numero.toFixed(2) + " Kelvin";
+          if(destino == 'c')
+            return kelvin.toCelsius().toFixed(2) + " Celsius";
+          if(destino == 'f')
+            return kelvin.toFahrenheit().toFixed(2) + " Fahrenheit";
+          break;
+        default:
+          return "ERROR Introduzca una temperatura valida: 330e-1 F to C";
+          
+      }
+
+    }
+    else
+      return "Introduzca una temperatura valida: 330e-1 F to C";
+  
+    };
         
-
-       
-
 })(this);
